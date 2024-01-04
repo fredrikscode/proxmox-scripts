843d4395a4cc76e63ee7c97285c8b010f56b125b
fredrikscode <fredrik@kihlstedt.io>
2024-01-04 16:48:54 +0100

    Some error handling and prints

--- templates.py
+++ templates.py
@@ -54,24 +54,30 @@ def customize_image(temp_dir, image_name):
         print("ERROR:", e)
 
 def create_template(vmid, name, image_name, template_storage, temp_dir, ssh_keyfile, username):
-    commands = [
-        ["qm", "create", vmid, "--name", name, "--ostype", "l26"],
-        ["qm", "set", vmid, "--net0", "virtio,bridge=vmbr0,tag=10"],
-        ["qm", "set", vmid, "--serial0", "socket", "--vga", "serial0"],
-        ["qm", "set", vmid, "--memory", "2048", "--cores", "2", "--cpu", "host"],
-        ["qm", "set", vmid, "--scsi0", f"{template_storage}:0,import-from={temp_dir}/{image_name},discard=on"],
-        ["qm", "set", vmid, "--boot", "order=scsi0", "--scsihw", "virtio-scsi-single"],
-        ["qm", "set", vmid, "--tablet", "0"],
-        ["qm", "set", vmid, "--agent", "enabled=1,fstrim_cloned_disks=1"],
-        ["qm", "set", vmid, "--ide2", f"{template_storage}:cloudinit"],
-        ["qm", "set", vmid, "--ipconfig0", "ip6=auto,ip=dhcp"],
-        ["qm", "set", vmid, "--sshkeys", ssh_keyfile],
-        ["qm", "set", vmid, "--ciuser", username],
-        ["qm", "template", vmid]
-    ]
+    try:
+        commands = [
+            ["qm", "create", vmid, "--name", name, "--ostype", "l26"],
+            ["qm", "set", vmid, "--net0", "virtio,bridge=vmbr0,tag=10"],
+            ["qm", "set", vmid, "--serial0", "socket", "--vga", "serial0"],
+            ["qm", "set", vmid, "--memory", "2048", "--cores", "2", "--cpu", "host"],
+            ["qm", "set", vmid, "--scsi0", f"{template_storage}:0,import-from={temp_dir}/{image_name},discard=on"],
+            ["qm", "set", vmid, "--boot", "order=scsi0", "--scsihw", "virtio-scsi-single"],
+            ["qm", "set", vmid, "--tablet", "0"],
+            ["qm", "set", vmid, "--agent", "enabled=1,fstrim_cloned_disks=1"],
+            ["qm", "set", vmid, "--ide2", f"{template_storage}:cloudinit"],
+            ["qm", "set", vmid, "--ipconfig0", "ip6=auto,ip=dhcp"],
+            ["qm", "set", vmid, "--sshkeys", ssh_keyfile],
+            ["qm", "set", vmid, "--ciuser", username],
+            ["qm", "template", vmid]
+        ]
 
-    for cmd in commands:
-        subprocess.check_call(cmd)
+        for cmd in commands:
+            subprocess.check_call(cmd)
+        return True
+    
+    except Exception as e:
+        print("ERROR:", e)
+        return False 
 
 def download_file(url, temp_dir, filename):
     response = requests.get(url, stream=True)
@@ -125,7 +131,10 @@ def main():
                 customize_image(temp_dir, image_name)
 
             print(f"[i] Creating template {name} ({vmid})")
-            create_template(vmid, name, image_name, template_storage, temp_dir, ssh_keyfile, username)
+            if create_template(vmid, name, image_name, template_storage, temp_dir, ssh_keyfile, username):
+                print("\033[32m✅ Template created: {}\033[0m".format(name))
+            else:
+                print("\033[31m❌ Template Creation Failed: {}\033[0m".format(name))
 
     os.remove(ssh_keyfile)
 
