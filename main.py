0353ca703e5a8293987d31c3f5b3b365430cb1a3
fredrikscode <fredrik@kihlstedt.io>
2024-01-05 09:00:50 +0100

    Started moving over to using new config

--- main.py
+++ main.py
@@ -3,22 +3,30 @@ import sys
 import os
 import socket
 from functions import (
-    runas_root, check_os, check_virt, check_tqdm, install_dependencies, check_and_delete_vm,
+    load_config, runas_root, check_os, check_virt, check_tqdm, install_dependencies, check_and_delete_vm,
     customize_image, create_template, download_file
 )
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Proxmox VM Template Creation Script")
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
-    parser.add_argument("-li", "--log-info", help="Enable logging INFO", action="store_true")
-    parser.add_argument("-ld", "--log-debug", help="Enable logging DEBUG", action="store_true")
 
     return parser.parse_args()
 
 def main():
     args = parse_arguments()
 
-    # Check prerequisites
+    config = load_config()
+    temporary_directory = config.get('default', 'temporary_directory', fallback='/tmp/')
+    ssh_pubkeys_file = temporary_directory + config.get('default', 'ssh_pubkeys_file', fallback='ssh_pubkeys')
+    username = "fredrik"
+    hostname = socket.gethostname()
+
+    # This is a bit ugly but is the current way to deal with hosts having different storage configurations for vm disks
+    if hostname not in config:
+        print(f"Configuration for hostname '{hostname}' not found.\nYou need to add it to the config file.\n\nExample:\n\n[FQDN]\ntemplate_storage = local-lvm\ntemplate_vmids = 1000,1001,1002\n")
+        sys.exit(1)
+
     if not check_os(args.verbose):
         print("\nThis script \033[4mcan only\033[0m be run on Proxmox VE.\n")
         sys.exit(1)
@@ -30,12 +38,6 @@ def main():
     if not check_virt(args.verbose) or not check_tqdm(args.verbose):
         print("\nThis script \033[4mrequires\033[0m \033[1mpython3-tqdm\033[0m and \033[1mvirt-customize\033[0m to worky.\n\n\033[1mInstall them with:\033[0m\napt install python3-tqdm libguestfs-tools\n")
         sys.exit(1)
-
-    # Configuration (TODO: Move to config file)
-    temp_dir = "/tmp"
-    ssh_keyfile = "/tmp/keys_internal_servers"
-    username = "fredrik"
-    hostname = socket.gethostname()
 
     config_mapping = {
         "titan.freddan.io": {"template_storage": "vm-storage", "template_vmids": ["3000", "3001", "3002"]},
@@ -56,23 +58,23 @@ def main():
         "debian12": f"{template_vmids[2]}|https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
     }
 
-    download_file("https://raw.githubusercontent.com/fredrikscode/ssh-keys/main/internal_servers", temp_dir, ssh_keyfile, args.verbose)
+    download_file("https://raw.githubusercontent.com/fredrikscode/ssh-keys/main/internal_servers", temporary_directory, ssh_pubkeys_file, args.verbose)
 
     for name, value in disk_images.items():
         vmid, url = value.split('|')
         image_name = os.path.basename(url)
 
         if check_and_delete_vm(vmid, name, args.verbose):
-            image_path = os.path.join(temp_dir, image_name)
+            image_path = os.path.join(temporary_directory, image_name)
             if not os.path.isfile(image_path):
-                download_file(url, temp_dir, image_name, args.verbose)
+                download_file(url, temporary_directory, image_name, args.verbose)
 
             if "ubuntu" in name or "debian" in name:
-                customize_image(temp_dir, image_name, name, args.verbose)
+                customize_image(temporary_directory, image_name, name, args.verbose)
 
-            create_template(vmid, name, image_name, template_storage, temp_dir, ssh_keyfile, username, args.verbose)
+            create_template(vmid, name, image_name, template_storage, temporary_directory, ssh_pubkeys_file, username, args.verbose)
 
-    os.remove(ssh_keyfile)
+    os.remove(ssh_pubkeys_file)
 
 if __name__ == "__main__":
     main()
