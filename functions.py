26a60e9c2edf893c5880fec34a137bd4d13f4439
fredrikscode <fredrik@kihlstedt.io>
2024-01-05 19:47:20 +0100

    asdad

--- functions.py
+++ functions.py
@@ -10,8 +10,8 @@ import threading
 import time
 import configparser
 
-logging.basicConfig(filename='pve-templates.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logHandler = RotatingFileHandler('pve-templates.log', maxBytes=10000000, backupCount=3)
+logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logHandler = RotatingFileHandler('debug.log', maxBytes=10000000, backupCount=3)
 logHandler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 
 class Spinner:
@@ -46,6 +46,7 @@ class Spinner:
 def load_config(config_path='config.ini'):
     config = configparser.ConfigParser()
     config.read(config_path)
+    logging.debug("Loaded config")
     return config
 
 def runas_root():
@@ -53,108 +54,114 @@ def runas_root():
         logging.error("Tried to run as non-root")
         return False
     else:
-        logging.info("Running as root")
+        logging.debug("Running as root")
         return True
 
-def check_os(verbose):
+def check_os():
     if 'pve' in platform.platform():
-        logging.info("Running on Proxmox VE")
+        logging.debug("Running on Proxmox VE")
         return True
     else:
         logging.error("Not running on Proxmox VE")
         return False
 
-def run_command(cmd, verbose):
-    if verbose:
+def run_command(cmd):
+        logging.debug(f"Asked to run command: {cmd}")
         subprocess.run(cmd, check=True)
-    else:
-        subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+        logging.debug(f"Ran command: {cmd}")
 
-def check_virt(verbose):
+def check_virt():
     try:
-        run_command(["virt-customize", "--version"], verbose)
-        logging.info("virt-customize is installed")
+        logging.debug(f"Checking if virt-customize is installed")
+        run_command(["virt-customize", "--version"])
+        logging.debug("virt-customize is installed")
         return True
     except (subprocess.CalledProcessError, FileNotFoundError):
-        if verbose:
-            print("\033[31m❌ virt-customize is not installed or not found in PATH\033[0m")
+        print("\033[31m❌ virt-customize is not installed or not found in PATH\033[0m")
         logging.error("virt-customize is not installed or not found in PATH")
         return False
 
-def check_tqdm(verbose):
+def check_tqdm():
     try:
+        logging.debug(f"Trying to import python3-tqdm")
         from tqdm import tqdm
-        logging.info("python3-tqdm is installed")
+        logging.debug("python3-tqdm is installed")
         return True
     
     except ImportError:
         logging.error("python3-tqdm is not installed")
         return False
-    
-    if verbose:
-        if 'tqdm' in sys.modules:
-            print("\033[32m✅ python3-tqdm is installed\033[0m")
-        else:
-            print("\033[31m❌ python3-tqdm is not installed\033[0m")
 
-def remove_file(path, filename, verbose):
+def remove_file(path, filename):
     try:
+        logging.debug(f"Trying to remove {filename} from {path}")
         os.remove(os.path.join(path, filename))
-        logging.info(f"Removed {filename} from {path}")
+        logging.debug(f"Removed {filename} from {path}")
 
     except FileNotFoundError:
         print(f"\033[31m❌ {filename} not found in {path}\033[0m")
+        logging.debug(f"Could not find file {filename} in path {path}")
 
-def install_dependencies(verbose):
+def install_dependencies():
     try:
-        run_command(["apt", "install", "-y", "python3-tqdm", "libguestfs-tools"], verbose)
+        run_command(["apt", "install", "-y", "python3-tqdm", "libguestfs-tools"])
         logging.info("Installed dependencies")
         return True
     except subprocess.CalledProcessError:
         logging.error("Failed to install dependencies")
         return False
 
-def check_and_delete_vm(vmid, name, verbose):
+def check_and_delete_vm(vmid, name):
     success_message = f"\033[32m✅ Removed template {name} [{vmid}]\033[0m"
     failure_message = f"\033[31m❌ Error while removing template {name} [{vmid}]\033[0m"
     spinner = Spinner(f"Checking if template {name} exists and deleting it if it does", success_message, failure_message)
     spinner.start()
     try:
-        run_command(["qm", "status", vmid], verbose)
+        logging.debug(f"Trying to check if template VM ({name} - {vmid}) exists")
+        run_command(["qm", "status", vmid])
     except subprocess.CalledProcessError:
         spinner.stop()
         return True
 
     try:
-        run_command(["qm", "destroy", vmid], verbose)
+        logging.debug(f"Trying to delete template VM ({name} - {vmid})")
+        run_command(["qm", "destroy", vmid])
         spinner.stop()
+        logging.info(f"Deleted template VM ({name} - {vmid})")
         return True
     except subprocess.CalledProcessError:
         spinner.stop()
+        logging.error(f"Failed to delete template VM ({name} - {vmid})")
         return False
     
-def customize_image(temporary_directory, image_name, name, verbose):
+def customize_image(temporary_directory, image_name, name):
     success_message = f"\033[32m✅ Customized image for {name}\033[0m"
     failure_message = f"\033[31m❌ Error while customizing image for {name}\033[0m"
     spinner = Spinner(f"Customizing image for {name}", success_message, failure_message)
     try:
+        logging.debug(f"Trying to customize {temporary_directory}/{image_name}")
         spinner.start()
-        run_command(["virt-customize", "-a", f"{temporary_directory}/{image_name}", "--firstboot-install", "qemu-guest-agent"], verbose)
-        logging.info(f"Added qemu-guest-agent installation on first boot in {image_name}")
-        run_command(["virt-customize", "-a", f"{temporary_directory}/{image_name}", "--firstboot-command", "systemctl enable --now qemu-guest-agent"], verbose)
-        logging.info(f"Enabled qemu-guest-agent on first boot in {image_name}")
+        run_command(["virt-customize", "-a", f"{temporary_directory}/{image_name}", "--firstboot-install", "qemu-guest-agent"])
+        logging.debug(f"Added qemu-guest-agent installation on first boot in {image_name}")
+        run_command(["virt-customize", "-a", f"{temporary_directory}/{image_name}", "--firstboot-command", "systemctl enable --now qemu-guest-agent"])
+        logging.debug(f"Enabled qemu-guest-agent on first boot in {image_name}")
     except Exception as e:
         logging.error(f"Failed to customize {image_name}")
         print("ERROR:", e)
     finally:
         spinner.stop()
 
-def create_template(vmid, name, image_name, template_storage, temporary_directory, ssh_keyfile, username, verbose):
+def create_template(vmid, name, image_name, template_storage, temporary_directory, ssh_pubkeys, cloudinit_user):
     success_message = f"\033[32m✅ Created template {name}\033[0m"
     failure_message = f"\033[31m❌ Error while creating template {name}\033[0m"
     spinner = Spinner(f"Trying to create template {name}", success_message, failure_message)
+
+    config = load_config()
+    cloudinit_user = config.get('default', 'cloudinit_user')
+
     try:
         spinner.start()
+        logging.debug(f"Trying to create template {name} [{vmid}]\nImage name: {image_name}\nTemplate storage: {template_storage}\nTemporary directory: {temporary_directory}\nSSH pubkeys: {ssh_pubkeys}\nCloudinit user: {cloudinit_user}")
         commands = [
             ["qm", "create", vmid, "--name", name, "--ostype", "l26"],
             ["qm", "set", vmid, "--net0", "virtio,bridge=vmbr0,tag=10"],
@@ -166,14 +173,15 @@ def create_template(vmid, name, image_name, template_storage, temporary_director
             ["qm", "set", vmid, "--agent", "enabled=1,fstrim_cloned_disks=1"],
             ["qm", "set", vmid, "--ide2", f"{template_storage}:cloudinit"],
             ["qm", "set", vmid, "--ipconfig0", "ip6=auto,ip=dhcp"],
-            ["qm", "set", vmid, "--sshkeys", ssh_keyfile],
-            ["qm", "set", vmid, "--ciuser", username],
+            ["qm", "set", vmid, "--sshkeys", ssh_pubkeys],
+            ["qm", "set", vmid, "--ciuser", cloudinit_user],
             ["qm", "template", vmid]
         ]
 
         for cmd in commands:
-            run_command(cmd, verbose)
-        logging.info(f"Successfully created template {name} [{vmid}]")
+            logging.debug(f"Running command: {cmd}")
+            run_command(cmd)
+        logging.debug(f"Successfully created template {name} [{vmid}]")
         return True
     
     except Exception as e:
@@ -184,21 +192,20 @@ def create_template(vmid, name, image_name, template_storage, temporary_director
     finally:
         spinner.stop()
     
-def download_file(url, verbose):
+def download_file(url):
     config = load_config()
     temporary_directory = config.get('default', 'temporary_directory')
     filename = os.path.basename(url)
-    if verbose:
-        response = requests.get(url, stream=True)
-        total_size = int(response.headers.get('content-length', 0))
-        block_size = 1024
-        with open(os.path.join(temporary_directory, filename), 'wb') as file, tqdm(total=total_size, unit='iB', unit_scale=True) as bar:
-            for data in response.iter_content(block_size):
-                bar.update(len(data))
-                file.write(data)
-    else:
-            response = requests.get(url, stream=True)
-            with open(os.path.join(temporary_directory, filename), 'wb') as file:
-                for data in response.iter_content(1024):
-                    file.write(data)
-            print("\033[32m✅ Finished downloading: {}\033[0m".format(filename))
+    # response = requests.get(url, stream=True)
+    # total_size = int(response.headers.get('content-length', 0))
+    # block_size = 1024
+    # with open(os.path.join(temporary_directory, filename), 'wb') as file, tqdm(total=total_size, unit='iB', unit_scale=True) as bar:
+    #     for data in response.iter_content(block_size):
+    #         bar.update(len(data))
+    #         file.write(data)
+    logging.debug(f"Trying to download {url} to {os.path.join(temporary_directory, filename)}")
+    response = requests.get(url, stream=True)
+    with open(os.path.join(temporary_directory, filename), 'wb') as file:
+        for data in response.iter_content(1024):
+            file.write(data)
+    print("\033[32m✅ Finished downloading: {}\033[0m".format(filename))
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import argparse
 import sys
 import os
 import socket
@@ -7,28 +6,24 @@ from functions import (
     customize_image, create_template, download_file, remove_file
 )
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Proxmox VM Template Creation Script")
-    parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
 
-    return parser.parse_args()
 
 def main():
-    args = parse_arguments()
 
     config = load_config()
     temporary_directory = config.get('default', 'temporary_directory', fallback='/tmp/')
-    ssh_pubkeys_file = config.get('default', 'ssh_pubkeys_file')
-    username = "fredrik"
+    ssh_pubkeys_url = config.get('default', 'ssh_pubkeys_url')
+    ssh_pubkeys_file = temporary_directory + ssh_pubkeys_url
+    disk_images = config.get('diskimages')
     hostname = socket.gethostname()
 
-    print(f"DEBUG: {hostname}")
     # This is a bit ugly but is the current way to deal with hosts having different storage configurations for vm disks
+    # TODO: Read storage dynamically from Proxmox API
     if hostname not in config:
         print(f"Configuration for hostname '{hostname}' not found.\nYou need to add it to the config file.\n\nExample:\n\n[FQDN]\ntemplate_storage = local-lvm\ntemplate_vmids = 1000,1001,1002\n")
         sys.exit(1)
 
-    if not check_os(args.verbose):
+    if not check_os():
         print("\nThis script \033[4mcan only\033[0m be run on Proxmox VE.\n")
         sys.exit(1)
 
@@ -36,44 +31,29 @@ def main():
         print("\nThis script \033[4mneeds\033[0m to be run as \033[1mroot\033[0m.\n")
         sys.exit(1)
 
-    if not check_virt(args.verbose) or not check_tqdm(args.verbose):
+    if not check_virt() or not check_tqdm():
         print("\nThis script \033[4mrequires\033[0m \033[1mpython3-tqdm\033[0m and \033[1mvirt-customize\033[0m to worky.\n\n\033[1mInstall them with:\033[0m\napt install python3-tqdm libguestfs-tools\n")
         sys.exit(1)
 
-    config_mapping = {
-        "titan.freddan.io": {"template_storage": "vm-storage", "template_vmids": ["3000", "3001", "3002"]},
-        "hive.freddan.io": {"template_storage": "local-lvm", "template_vmids": ["2000", "2001", "2002"]},
-        "nano.freddan.io": {"template_storage": "local-lvm", "template_vmids": ["1000", "1001", "1002"]}
-    }
-
-    config = config_mapping.get(hostname)
     if not config:
         print(f"Configuration for hostname '{hostname}' not found.")
         sys.exit(1)
-    template_storage = config["template_storage"]
-    template_vmids = config["template_vmids"]
 
-    disk_images = {
-        "alma9.3": f"{template_vmids[0]}|https://repo.almalinux.org/almalinux/9.3/cloud/x86_64/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2",
-        "ubuntu20.04": f"{template_vmids[1]}|https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img",
-        "debian12": f"{template_vmids[2]}|https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
-    }
-
-    download_file(url=ssh_pubkeys_file, verbose=args.verbose)
+    download_file(url=ssh_pubkeys_url)
 
     for name, value in disk_images.items():
         vmid, url = value.split('|')
         image_name = os.path.basename(url)
 
-        if check_and_delete_vm(vmid, name, args.verbose):
+        if check_and_delete_vm(vmid, name, ):
             image_path = os.path.join(temporary_directory, image_name)
             if not os.path.isfile(image_path):
-                download_file(url, temporary_directory, image_name, args.verbose)
+                download_file(url, temporary_directory, image_name)
 
             if "ubuntu" in name or "debian" in name:
-                customize_image(temporary_directory, image_name, name, args.verbose)
+                customize_image(temporary_directory, image_name, name)
 
-            create_template(vmid, name, image_name, template_storage, temporary_directory, ssh_pubkeys_file, username, args.verbose)
+            create_template(vmid, name, image_name, template_storage, temporary_directory, ssh_pubkeys_file, cloudinit_user)
 
     remove_file(temporary_directory)
 
